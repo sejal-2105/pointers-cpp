82a1884362b1f9ecf0edb5af2563c6d5dbbb0200
sejal-2105 <sejalsingh5216@gmail.com>
2025-08-05 15:04:30 +0530

    Add files via upload

---
+++ pointers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int*aptr;
+    aptr=&a;
+    cout<<a<<endl;
+    cout<<*aptr<<endl;
+    cout<<aptr<<endl;
+    cout<<&a<<endl;
+    return 0;
+}
